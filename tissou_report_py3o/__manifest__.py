32a2f89a886b1fac66edbb1c2d148138fb94dc44
Laetitia Da Costa <laetitia.dacosta@elabore.coop>
2024-03-07 17:23:17 +0100

    [ADD]tissou_report_py3o

---
+++ tissou_report_py3o/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Le Tissou Reports Py3o',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Le Tissou py3o reports',
+    'description': """
+Le Tissou Reports Py3o
+=================
+
+    """,
+    'author': 'Elabore',
+    'depends': [
+        'report_py3o','base','membership',
+        ],
+    'data': ['config.xml','report.xml'],
+    'installable': True,
+}
